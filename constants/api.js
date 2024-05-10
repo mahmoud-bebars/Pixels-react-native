529788378791bff5786f727570d9e999480d1fda
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2024-05-10 09:14:18 +0300

    fixing env varaibles setup

--- constants/api.js
+++ constants/api.js
@@ -1,3 +1,3 @@
-export const API_KEY = process.env.API_KEY;
+export const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
 
-export const URL = process.env.URL;
+export const URL = process.env.EXPO_PUBLIC_URL;
